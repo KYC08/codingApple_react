41648c4c131900df6bf947c5c012fbe0f450ea0c
KYC08 <dkswoah127@naver.com>
2024-11-30 18:24:33 +0900

    초기작업물

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+/* 어떤걸 컴포넌트로 만들면 좋은가
+    1. 반복적인 html 축약할 때
+    2. 큰 페이지들
+    3. 자주 변경되는 것들*/
+
+/* 동적인 UI 만드는 step
+    1. html css로 미리 디자인 완성
+    2. UI의 현재 상태를 state로 저장
+    3. state에 따라 UI가 어떻게 보일지 작성*/
 
 function App() {
+  let post = '역삼 우동 맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+        }}>
+        정렬버튼
+      </button>
+
+      <div className="list">
+        <h4>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}>
+            👍
+          </span>
+          {따봉}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4
+          onClick={() => {
+            modal == true ? setModal(false) : setModal(true);
+          }}>
+          {글제목[2]}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      {modal == true ? <Modal /> : null}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
