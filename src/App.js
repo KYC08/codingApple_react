132f374ca259626744f80c0f8efd67364133f409
KYC08 <dkswoah127@naver.com>
2024-12-01 13:09:53 +0900

    코딩애플 리액트 블로그

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import {useState} from "react";
 
 /* 어떤걸 컴포넌트로 만들면 좋은가
     1. 반복적인 html 축약할 때
@@ -12,10 +12,16 @@ import { useState } from 'react';
     3. state에 따라 UI가 어떻게 보일지 작성*/
 
 function App() {
-  let post = '역삼 우동 맛집';
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let post = "역삼 우동 맛집";
+  let [글제목, 글제목변경] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬독학",
+  ]);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState("");
 
   return (
     <div className="App">
@@ -23,54 +29,96 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
+      {글제목.map(function (a, i) {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                setModal(true);
+                setTitle(i);
+              }}
+            >
+              {글제목[i]}
+              <span
+                onClick={() => {
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy);
+                }}
+              >
+                👍
+              </span>
+              {따봉[i]}
+            </h4>
+            <p>2월 17일 발행</p>
+            <button
+              onClick={() => {
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        );
+      })}
+
+      <input
+        onChange={(e) => {
+          입력값변경(e.target.value);
+          console.log(입력값);
+        }}
+      />
       <button
         onClick={() => {
           let copy = [...글제목];
-          copy.sort();
+          copy.unshift(입력값);
           글제목변경(copy);
-        }}>
-        정렬버튼
+        }}
+      >
+        안녕
       </button>
 
-      <div className="list">
-        <h4>
-          {글제목[0]}
-          <span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}>
-            👍
-          </span>
-          {따봉}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            modal == true ? setModal(false) : setModal(true);
-          }}>
-          {글제목[2]}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      {modal == true ? <Modal /> : null}
+      {modal == true ? <Modal title={title} 글제목={글제목} /> : null}
+      <Modal2></Modal2>
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글수정</button>
     </div>
   );
+}
+
+class Modal2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "kim",
+      age: 20,
+    };
+  }
+  render() {
+    return (
+      <div>
+        안녕{this.state.age}
+        <button
+          onClick={() => {
+            this.setState({age: 21});
+          }}
+        >
+          버튼
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
